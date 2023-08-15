360ef11f8ea4702c70dacfeecd766a5679d8b16c
Dev Agrawal <agrawadv@mail.uc.edu>
2023-08-15 00:25:47 -0400

    logged out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import { Task } from "@/entities/Task";
-import { useUser, useOrganization, useClerk } from "@clerk/nextjs";
+import {
+  useUser,
+  useOrganization,
+  useClerk,
+  SignInButton,
+} from "@clerk/nextjs";
 import { useQueryClient, useQuery } from "@tanstack/react-query";
 import clsx from "clsx";
 import { useState, useEffect, FormEvent } from "react";
@@ -15,7 +20,7 @@ export default function Home() {
   const [_selectedProject, setSelected] = useLocalStorage("selectedProject");
 
   const qc = useQueryClient();
-  const { user } = useUser();
+  const { user, isLoaded } = useUser();
   const { organization } = useOrganization();
 
   const selectedProject = organization ? _selectedProject : undefined;
@@ -79,57 +84,79 @@ export default function Home() {
       <main>
         <h1 className="text-[#ef4444] italic text-6xl text-center">todos</h1>
 
-        <div className=" w-1/2 min-w-[400px] bg-white border border-solid border-gray-300 rounded-lg m-auto card-shadow ">
-          <div className="flex">
-            {organization && (
-              <>
-                {projects?.length ? (
-                  projects.map((project) => (
-                    <button
-                      key={project.id}
-                      onClick={() => setSelected(project.id)}
-                      className={clsx(
-                        "px-3 py-2 border-x bg-gray-100 text-gray-700",
-                        selectedProject === project.id &&
-                          "bg-white text-black text-xl"
-                      )}
-                    >
-                      {project.title}
-                    </button>
-                  ))
-                ) : (
-                  <div className="px-3 py-2 text-gray-500">
-                    No projects found
-                  </div>
-                )}
-                <span className="grow"></span>
-                <button className="px-3 py-2 border-x" onClick={addProject}>
-                  Add Project
-                </button>
-              </>
-            )}
+        {!isLoaded && (
+          <div className=" w-1/2 min-w-[400px] bg-white border border-solid border-gray-300 rounded-lg m-auto card-shadow ">
+            <div className="flex">
+              <div className="px-3 py-2 text-gray-500">Loading...</div>
+            </div>
           </div>
-          <form
-            onSubmit={addTask}
-            className="border border-b border-solid flex px-2 py-1 items-center todo"
-          >
-            <input
-              value={newTaskTitle}
-              placeholder="What needs to be done?"
-              onChange={(e) => setNewTaskTitle(e.target.value)}
-              className="w-full p-1 outline-none placeholder:italic"
-            />
-            <button className="font-medium rounded-full border px-4 py-2 text-md font-sans bg-white border-none text-white hover:text-inherit hover:bg-gray-200">
-              Add
-            </button>
-          </form>
+        )}
+        {isLoaded && !user && (
+          <div className=" w-1/2 min-w-[400px] bg-white border border-solid border-gray-300 rounded-lg m-auto card-shadow ">
+            <div className="flex">
+              <div className="px-3 py-2 text-gray-500">
+                Please{" "}
+                <SignInButton>
+                  <button className="underline">sign in</button>
+                </SignInButton>{" "}
+                to continue
+              </div>
+            </div>
+          </div>
+        )}
+        {isLoaded && user && (
+          <div className=" w-1/2 min-w-[400px] bg-white border border-solid border-gray-300 rounded-lg m-auto card-shadow ">
+            <div className="flex">
+              {organization && (
+                <>
+                  {projects?.length ? (
+                    projects.map((project) => (
+                      <button
+                        key={project.id}
+                        onClick={() => setSelected(project.id)}
+                        className={clsx(
+                          "px-3 py-2 border-x bg-gray-100 text-gray-700",
+                          selectedProject === project.id &&
+                            "bg-white text-black text-xl"
+                        )}
+                      >
+                        {project.title}
+                      </button>
+                    ))
+                  ) : (
+                    <div className="px-3 py-2 text-gray-500">
+                      No projects found
+                    </div>
+                  )}
+                  <span className="grow"></span>
+                  <button className="px-3 py-2 border-x" onClick={addProject}>
+                    Add Project
+                  </button>
+                </>
+              )}
+            </div>
+            <form
+              onSubmit={addTask}
+              className="border border-b border-solid flex px-2 py-1 items-center todo"
+            >
+              <input
+                value={newTaskTitle}
+                placeholder="What needs to be done?"
+                onChange={(e) => setNewTaskTitle(e.target.value)}
+                className="w-full p-1 outline-none placeholder:italic"
+              />
+              <button className="font-medium rounded-full border px-4 py-2 text-md font-sans bg-white border-none text-white hover:text-inherit hover:bg-gray-200">
+                Add
+              </button>
+            </form>
 
-          {isLoading && <div>Loading...</div>}
+            {isLoading && <div>Loading...</div>}
 
-          {tasks?.map((task) => (
-            <TaskComponent task={task} key={task.id} />
-          ))}
-        </div>
+            {tasks?.map((task) => (
+              <TaskComponent task={task} key={task.id} />
+            ))}
+          </div>
+        )}
       </main>
     </Layout>
   );
